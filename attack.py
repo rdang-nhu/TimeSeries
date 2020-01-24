cadaf6434656b2591a9c290a92a94aa8622892c7
rdang-nhu <raphael.dang-nhu@polytechnique.edu>
2020-01-24 19:02:10 +0100

    Pushing to server

--- attack.py
+++ attack.py
@@ -417,9 +417,12 @@ class Attack():
                 ax[k].plot(x[self.params.predict_start:],
                            zero_mu_chosen[k], color='brown')
 
+                double_pert = (1+best_perturbation["double"][tolerance][random_sample][k])
+                zero_pert = (1 + best_perturbation["zero"][tolerance][random_sample][k])
+                print(double_pert.shape)
 
-                ax[k].plot(x[:self.params.predict_start], label_plot[k, :self.params.predict_start]*(1+best_perturbation["double"][tolerance][random_sample][k]), color='y')
-                ax[k].plot(x[:self.params.predict_start:], label_plot[k, :self.params.predict_start] * (1 + best_perturbation["zero"][tolerance][random_sample][k]), color='purple')
+                ax[k].plot(x[:self.params.predict_start], label_plot[k, :self.params.predict_start]*double_pert, color='y')
+                ax[k].plot(x[:self.params.predict_start:], label_plot[k, :self.params.predict_start] * , color='purple')
 
                 ax[k].axhline(plot_target_double, color='orange', linestyle='dashed')
                 ax[k].axhline(plot_target_zero, color='orange', linestyle='dashed')
