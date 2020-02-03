a8374b3b89468da94c02a16961a3fb3df5eebcaa
rdang-nhu <raphael.dang-nhu@polytechnique.edu>
2020-02-03 10:55:29 +0100

    Pushing to server

--- model/net.py
+++ model/net.py
@@ -133,7 +133,7 @@ class Net(nn.Module):
                 id_batch, decoder_hidden, decoder_cell)
 
             print("sample shape",sample[t].shape)
-            print("v shape",v_shape.shape)
+            print("v shape",v_batch.shape)
             normalized_value = (sample[t] - v_batch[:,1])*v_batch[:,0]
 
             ret = ONEOVERSQRT2PI * torch.exp(-0.5 * ((normalized_value - mu_de) / sigma_de) ** 2) / sigma_de
