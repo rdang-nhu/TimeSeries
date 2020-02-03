46095da04084baa4615aa8da8ec29ca920a6152e
rdang-nhu <raphael.dang-nhu@polytechnique.edu>
2020-02-03 11:55:54 +0100

    Pushing to server

--- attack.py
+++ attack.py
@@ -90,8 +90,6 @@ class AttackModule(nn.Module):
                                             hidden,
                                             cell)
 
-        print("hidden grad", hidden.requires_grad)
-
         for i in range(samples.shape[0]):
             sample = samples[i]
             log_prob = attack_utils.forward_log_prob(model,
