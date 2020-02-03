a196b53d8ea5db2920236f4b00248bc62326bd84
rdang-nhu <raphael.dang-nhu@polytechnique.edu>
2020-02-03 12:06:30 +0100

    Pushing to server

--- attack.py
+++ attack.py
@@ -191,7 +191,7 @@ class Attack():
         attack_module.perturbation.grad *= mean.grad[:,self.params.target]
 
         detached_pert = attack_module.perturbation.grad.detach().cpu().numpy()
-        print("attack pert grad 1", detached_pert[detached_pert != 0.])
+        print("attack pert grad 1", detached_pert[detached_pert != 0.].shape)
 
         # Compute the derivative of the loss with respect to the norm
         mean.requires_grad = False
